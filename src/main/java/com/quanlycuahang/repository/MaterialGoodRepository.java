ceabcf28c379ec11c008335676e9c1636806b3ac
khoi <trinhdangkhoi2104@gmail.com>
2021-05-17 22:44:41 +0700

    .

--- src/main/java/com/quanlycuahang/repository/MaterialGoodRepository.java
+++ src/main/java/com/quanlycuahang/repository/MaterialGoodRepository.java
@@ -8,13 +8,13 @@ import org.springframework.data.repository.CrudRepository;
 import com.quanlycuahang.entity.MaterialGood;
 
 public interface MaterialGoodRepository extends CrudRepository<MaterialGood, Long>, MaterialGoodRepositoryCustom{
-	@Query(value = "select count(*) from MaterialGoods where materialGoodCode = ?1", nativeQuery = true)
+	@Query(value = "select count(*) from MaterialGoods where materialGoodCode = ?1 ; ", nativeQuery = true)
 	Integer checkDuplicateCode(String code);
 	
-	@Query(value = "select * from MaterialGoods", nativeQuery = true)
+	@Query(value = "select * from MaterialGoods ; ", nativeQuery = true)
 	List<MaterialGood> findAll();
 	
-	@Query(value = "select count(*) from PPOrderDetail where materialGoodID = ?1", nativeQuery = true)
+	@Query(value = "select count(1) from PPOrderDetail where materialGoodID = ?1 ; ", nativeQuery = true)
 	Integer checkRef(String id);
 
 	
