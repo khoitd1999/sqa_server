587930fa8d83cc71ba738d348081ec47a07e4cff
khoi <trinhdangkhoi2104@gmail.com>
2021-05-17 20:28:57 +0700

    .

--- src/main/java/com/quanlycuahang/repository/UserStoreRepository.java
+++ src/main/java/com/quanlycuahang/repository/UserStoreRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.CrudRepository;
 import com.quanlycuahang.entity.UserStore;
 
 public interface UserStoreRepository extends CrudRepository<UserStore, Long>{
-	@Query(value = "select * from UserStore where username = ?1 and password = ?2", nativeQuery = true)
+	@Query(value = "select * from UserStore where username = ?1  and password = ?2", nativeQuery = true)
 	UserStore checkUser(String username, String password);
 	
 	@Query(value = "select * from UserStore where role = 'NV'", nativeQuery = true)
